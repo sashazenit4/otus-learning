f75f063da2dc6986c1126849dec84bb0581caadb
Aleksandr Kholin <holinshura@gmail.com>
2024-08-27 11:26:30 +0300

    refactor(test): refactor deal update to services

--- to-d7.php
+++ to-d7.php
@@ -1,13 +1,22 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
 
+use Bitrix\Crm\Service\Container;
+
 \Bitrix\Main\Loader::includeModule('crm');
-$dealFields = [
-    'TITLE' => 'Тестовая сделка',
-    'STAGE_ID' => 'C2:PREPARATION',
-];
-$idDeal = 15;
-$updateResult = \Aholin\Crm\DealTable::update($idDeal, $dealFields);
+
+$idDeal = 9;
+
+$dealFactory = Container::getInstance()->getFactory(\CCrmOwnerType::Deal);
+
+$dealItem = $dealFactory->getItem($idDeal);
+
+$dealItem->set('TITLE', 'Тестовая сделка');
+$dealItem->set('STAGE_ID', 'C2:PREPARATION');
+
+$operation = $dealFactory->getUpdateOperation($dealItem);
+
+$updateResult = $operation->launch();
 
 if ($updateResult->isSuccess()) {
     echo 'Сделка успешно обновлена';
